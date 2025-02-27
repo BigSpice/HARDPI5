3204744b5a10b2e0a453ed0c3e85700001024317
Juicy <jordan.k.m@proton.me>
2025-02-27 14:01:33 -0500

    Update Piezo_Read_Final.ino

--- Piezo_Read_Final.ino
+++ Piezo_Read_Final.ino
@@ -1,5 +1,12 @@
+//This Sketch is intended for the Nano Platform
+//ATmega328P Flasher
+//Pins to used are A0 and D2
+//1khom Resitor to prevent voltage drift
+
 const int PIEZO_PIN = A0; // Piezo output
-//1kOhm Resistor across the Negative and Postive Leads then connect as normal;
+const int LED_CAMERA = 2; // IR output
+
+//1k    Ohm Resistor across the Negative and Postive Leads then connect as normal;
 //eg; breadboard config of a 2x6 grid on the bread board.
      ////////Breadboard Layout////////
                 //.0.0.//
@@ -10,20 +17,20 @@ const int PIEZO_PIN = A0; // Piezo output
      //|    1Kohm + | 1kohm -     |//
      //|Jumper to A0|Jumper to GND|//
      ////////////////////////////////
+//Connect the Piezo Hardware Flasher (LED of any IR type) to PIN 2 Directed toward the Observer Camera
 //Ensure you use the same GND rail(same side pin)//
+
 void setup() 
 {
-
-  Serial.begin(2000000);
+  Serial.begin(230400); //Make sure any connected serial observing tools use this baud rate for communication
+  pinMode(LED_CAMERA, OUTPUT); //pinmode config
 }
 
 void loop() 
 {
+
   // Read Piezo ADC value in, and convert it to a voltage
-  int piezoADC = analogRead(PIEZO_PIN);
-  float piezoV = piezoADC / 1023.0 * 550.0;
+  float piezoV = analogRead(PIEZO_PIN) / 1023.0 * 550.0; //1023 is resoloution
+  digitalWrite(LED_CAMERA, (piezoV >= 3.9));  //Hard Bottleneck
   Serial.println(piezoV); // Print the voltage.
-  if(piezoV > 0.10){Serial.println("HIT - VAL ->" + String(piezoV)); delay(1000);  }
-  // Print the voltage.
-  delay(1);
 }
